7c5bee4bfb9cd90c06537465ff1aebb96734d70c
Bartek Z <bartekzarzyk@gmail.com>
2023-07-10 19:52:14 +0200

    remove cards from Liked

--- src/app/polubione/page.tsx
+++ src/app/polubione/page.tsx
@@ -6,10 +6,7 @@ const Liked = () => {
       <div>
         <PageTitle title='Polubione Ogłoszenia' />
         <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6'>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {/* add here liked cards */}
         </div>
       </div>
     </PageWrapper>
