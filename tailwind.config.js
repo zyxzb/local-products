e1cfdb5466f7729e1461b0b33b744012ebf0e4e2
Bartek Z <bartekzarzyk@gmail.com>
2023-07-13 16:30:41 +0200

    try to fix vercel error

--- tailwind.config.js
+++ tailwind.config.js
@@ -33,7 +33,7 @@ module.exports = {
       },
       boxShadow: {
         label:
-          'rgba(17, 17, 26, 0.1) 0px 4px 16px, rgba(17, 17, 26, 0.05) 0px 8px 32px;',
+          'rgba(17, 17, 26, 0.1) 0px 4px 16px, rgba(17, 17, 26, 0.05) 0px 8px 32px',
       },
     },
   },
