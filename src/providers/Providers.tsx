685ef2eaa3be05f978899a1267fce6024a63adf9
Bartek Z <bartekzarzyk@gmail.com>
2023-11-06 11:25:57 +0100

    remove next-toploader package

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -1,5 +1,4 @@
 import { CreateAdContextProvider } from '@/context/createAddContext';
-import NextTopLoader from 'nextjs-toploader';
 import { ToastContainer } from 'react-toastify';
 
 import 'react-toastify/dist/ReactToastify.css';
@@ -7,7 +6,6 @@ import 'react-toastify/dist/ReactToastify.css';
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <NextTopLoader showSpinner={false} shadow={false} color='#adc698' />
       <CreateAdContextProvider>{children}</CreateAdContextProvider>
       <ToastContainer />
     </>
